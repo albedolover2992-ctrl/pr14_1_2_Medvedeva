daae3a68cf7f329f49e58c9babfc3d4702068011
albedolover2992-ctrl <albedolover2992@gmail.com>
2026-03-13 13:00:13 +0500

    Add files via upload

---
+++ Практическая 14.1. Алгоритмизация/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Практическая_14._Алгоритмизация
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                Stack<int> nums = new Stack<int>(n);
+                for (int i = 1; i <= n; i++)
+                {
+                    nums.Push(i);
+                }
+                Console.WriteLine($"n = {n}" +
+                    $"\nРазмерность стека: {nums.Count}" +
+                    $"\nВверхний элемент стека: {nums.Peek()}"+ 
+                    $"\nРазмерность стека: {nums.Count}" +
+                    $"\nСодержимое стека: ");
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.Write($"{nums.Pop()} ");
+                }
+                nums.Clear();
+                Console.WriteLine($"\nНовая размерность стека: {nums.Count}");
+  
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Ошибка");
+            }
+       }
+    }
+}
